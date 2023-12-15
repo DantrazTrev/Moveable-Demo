3e5b163b7f43572ef71bcad6ddc7b995bf4f31d2
DantrazTrev <ayushpratap16@gmail.com>
2023-12-15 18:56:11 +0530

    Update edit button controls

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,11 @@ const Navbar:React.FC<NavbarProps> = ({toggleEditor,editMode}) => {
     return (
         <nav className="bg-gray-100 text-black h-14 w-screen flex flex-row justify-between items-center px-8">
             <ul className="flex justify-center space-x-4">
-                <li>Demo Dan</li>
+                <li>Currently in {!editMode?'Preview':'Edit'} Mode</li>
             </ul>
-            <ul className="flex justify-end space-x-4 cursor-pointer hover:underline">
+            <ul className="flex justify-end space-x-4 cursor-pointer transition-all">
                 <li
+                className='bg-slate-900 text-white py-2 px-3 hover:bg-slate-800 rounded'
                 onClick={toggleEditor}
                 >
                     {editMode?'Preview':'Edit'}
